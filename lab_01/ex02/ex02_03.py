608470d386d9d7f551430f7257dde41f869fa7e0
Developer Team <team@drivemonitoring.com>
2026-06-17 21:50:30 +0700

    Restore lab_01 folder from Lab_01 branch

---
+++ lab_01/ex02/ex02_03.py
@@ -0,0 +1,5 @@
+so =int(input("nhap mot so nguyen: "))
+if( so %2==0):
+    print(f"{so} vua nhap la so chan")
+else:
+    print(f"{so} vua nhap la so le")
---
+++ lab_01/ex03/ex03_02.py
@@ -0,0 +1,5 @@
+def dao_chuoi(chuoi):
+    return chuoi[::-1]
+
+
+print(" List sau khi dao nguoc: ", dao_chuoi(input("Nhap vao mot chuoi: ")))
